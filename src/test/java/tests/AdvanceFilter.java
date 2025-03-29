ed0876f10cf27d282b140cca817656131424f856
aiswaryavin <aiswaryakrishnan100@gmail.com>
2025-03-29 11:39:10 +0400

    Added Advance Filter Page and updated tests

---
+++ src/test/java/tests/AdvanceFilter.java
@@ -0,0 +1,13 @@
+package tests;
+
+import POM.AdvanceFilterPage;
+import org.testng.annotations.Test;
+
+public class AdvanceFilter {
+    @Test(priority=4)
+    public void filter(){
+        AdvanceFilterPage advance=new AdvanceFilterPage();
+        git
+
+    }
+}
